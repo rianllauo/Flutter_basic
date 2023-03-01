7ff23da07b74e019b90395612eecea2343b1976e
rianllauo <rianllauo@gmail.com>
2023-03-01 14:56:47 +0700

    layouting

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
                   Container(
                     width: 100,
                     height: 100,
-                    color: Colors.amber,
+                    color: Color(0xFFFFC107),
                     child: Center(child: Text('Kuning')),
                   ),
                   Container(
