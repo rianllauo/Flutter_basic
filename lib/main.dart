ff15c13169e94f8209b2ee7aee2b859eae9a73bb
rianllauo <rianllauo@gmail.com>
2023-03-01 23:14:57 +0700

    bottom navbar

--- lib/main.dart
+++ lib/main.dart
@@ -23,46 +23,48 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
-  late TabController tabController = TabController(length: 4, vsync: this);
+class _HomePageState extends State<HomePage> {
+  int? indexTab;
+
+  late List showWidget = [
+    Center(
+      child: Text('Home'),
+    ),
+    Center(
+      child: Text('Cart'),
+    ),
+    Center(
+      child: Text('User'),
+    ),
+  ];
+
+  @override
+  void initState() {
+    indexTab = 0;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           toolbarHeight: 70,
-          backgroundColor: Colors.teal,
-          title: const Text('WhatsApp'),
-          bottom: TabBar(controller: tabController, tabs: [
-            Tab(
-              icon: Icon(Icons.camera_alt_rounded),
-            ),
-            Tab(
-              text: 'Chats',
-            ),
-            Tab(
-              text: 'Status',
-            ),
-            Tab(
-              text: 'Calls',
-            ),
-          ]),
+          backgroundColor: Colors.blue,
+          title: const Text('Bottom Navigation Bar'),
         ),
-        body: TabBarView(
-          controller: tabController,
-          children: [
-            Center(
-              child: Text('Camera'),
-            ),
-            Center(
-              child: Text('Chats'),
-            ),
-            Center(
-              child: Text('Status'),
-            ),
-            Center(
-              child: Text('Calls'),
-            ),
+        body: showWidget[indexTab!],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: indexTab!,
+          selectedItemColor: Colors.red,
+          unselectedItemColor: Colors.black,
+          onTap: (value) => setState(() {
+            indexTab = value;
+          }),
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.shopping_cart), label: 'Cart'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'User'),
           ],
         ));
   }
