1a50446dab834b6567058d26d15b32f7ae77a004
rianllauo <rianllauo@gmail.com>
2023-03-01 21:15:12 +0700

    text_field

--- lib/main.dart
+++ lib/main.dart
@@ -5,20 +5,104 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('First App'),
-          centerTitle: true,
-        ),
-        body: const Center(
-          child: Text(
-            'Hello World!',
-            style: TextStyle(fontSize: 28),
-          ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isPasswordHidden = true;
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Text Field'),
+      ),
+      body: Center(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+          children: [
+            TextField(
+              onChanged: (value) => print(value),
+              controller: emailController,
+              textInputAction: TextInputAction.next,
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.email),
+                labelText: 'Email',
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(13),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              controller: passwordController,
+              textInputAction: TextInputAction.next,
+              autocorrect: false,
+              obscureText: isPasswordHidden,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.vpn_key),
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    if (isPasswordHidden) {
+                      setState(() {
+                        isPasswordHidden = false;
+                      });
+                    } else {
+                      setState(() {
+                        isPasswordHidden = true;
+                      });
+                    }
+                  },
+                  icon: Icon(Icons.remove_red_eye),
+                ),
+                labelText: 'Email',
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(13),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print(
+                    'Email: ${emailController.text}, password: ${passwordController.text} ');
+              },
+              child: Text('Login'),
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 12),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(13),
+                  )),
+            )
+          ],
         ),
       ),
     );
