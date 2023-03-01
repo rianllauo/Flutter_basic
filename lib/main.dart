e9edbcd4d0b5cf5f5e0304243a84f3707060021b
rianllauo <rianllauo@gmail.com>
2023-03-01 13:23:55 +0700

    counter_app

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,73 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int nilai = 0;
+
+  void increment() {
+    setState(() {
+      nilai = nilai + 1;
+    });
+  }
+
+  void decrement() {
+    if (nilai > 0) {
+      setState(() {
+        nilai = nilai - 1;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('First App'),
-          centerTitle: true,
+          title: const Text('Counter App'),
         ),
-        body: const Center(
-          child: Text(
-            'Hello World!',
-            style: TextStyle(fontSize: 28),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '$nilai',
+                style: const TextStyle(fontSize: 34),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      decrement();
+                    },
+                    child: const Icon(Icons.remove),
+                  ),
+                  const SizedBox(
+                    width: 13,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      increment();
+                    },
+                    child: const Icon(Icons.add),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
