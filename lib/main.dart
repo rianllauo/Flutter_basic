05042dd5e2cb551f4d14d967886b710494f306f3
rianllauo <rianllauo@gmail.com>
2023-03-01 21:57:59 +0700

    tab bar

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
@@ -22,89 +23,47 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
-  bool isPasswordHidden = true;
-  TextEditingController emailController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  late TabController tabController = TabController(length: 4, vsync: this);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Text Field'),
-      ),
-      body: Center(
-        child: ListView(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
-          children: [
-            TextField(
-              onChanged: (value) => print(value),
-              controller: emailController,
-              textInputAction: TextInputAction.next,
-              autocorrect: false,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.email),
-                labelText: 'Email',
-                contentPadding:
-                    EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(13),
-                ),
-              ),
+        appBar: AppBar(
+          toolbarHeight: 70,
+          backgroundColor: Colors.teal,
+          title: const Text('WhatsApp'),
+          bottom: TabBar(controller: tabController, tabs: [
+            Tab(
+              icon: Icon(Icons.camera_alt_rounded),
             ),
-            const SizedBox(
-              height: 20,
+            Tab(
+              text: 'Chats',
             ),
-            TextField(
-              controller: passwordController,
-              textInputAction: TextInputAction.next,
-              autocorrect: false,
-              obscureText: isPasswordHidden,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.vpn_key),
-                suffixIcon: IconButton(
-                  onPressed: () {
-                    if (isPasswordHidden) {
-                      setState(() {
-                        isPasswordHidden = false;
-                      });
-                    } else {
-                      setState(() {
-                        isPasswordHidden = true;
-                      });
-                    }
-                  },
-                  icon: Icon(Icons.remove_red_eye),
-                ),
-                labelText: 'Email',
-                contentPadding:
-                    EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(13),
-                ),
-              ),
+            Tab(
+              text: 'Status',
             ),
-            const SizedBox(
-              height: 20,
+            Tab(
+              text: 'Calls',
             ),
-            ElevatedButton(
-              onPressed: () {
-                print(
-                    'Email: ${emailController.text}, password: ${passwordController.text} ');
-              },
-              child: Text('Login'),
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.red,
-                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 12),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(13),
-                  )),
-            )
-          ],
+          ]),
         ),
-      ),
-    );
+        body: TabBarView(
+          controller: tabController,
+          children: [
+            Center(
+              child: Text('Camera'),
+            ),
+            Center(
+              child: Text('Chats'),
+            ),
+            Center(
+              child: Text('Status'),
+            ),
+            Center(
+              child: Text('Calls'),
+            ),
+          ],
+        ));
   }
 }
