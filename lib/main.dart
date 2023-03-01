15850b3df06a8635248d9851d8e233ded9d8a024
rianllauo <rianllauo@gmail.com>
2023-03-01 14:00:06 +0700

    layouting

--- lib/main.dart
+++ lib/main.dart
@@ -5,22 +5,58 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('First App'),
-          centerTitle: true,
-        ),
-        body: const Center(
-          child: Text(
-            'Hello World!',
-            style: TextStyle(fontSize: 28),
+          appBar: AppBar(
+            title: const Text('Layouting'),
           ),
-        ),
-      ),
+          body: Container(
+            width: double.infinity,
+            child: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.amber,
+                    child: Center(child: Text('Kuning')),
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.blue,
+                    child: Center(child: Text('blue')),
+                  ),
+                  Container(
+                    width: 300,
+                    height: 300,
+                    color: Colors.green,
+                    child: Center(child: Text('hijau')),
+                  ),
+                  Container(
+                    width: 200,
+                    height: 200,
+                    color: Colors.red,
+                    child: Center(child: Text('hijau')),
+                  ),
+                  Container(
+                    width: 200,
+                    height: 200,
+                    color: Colors.indigo,
+                    child: Center(child: Text('hijau')),
+                  )
+                ],
+              ),
+            ),
+          )),
     );
   }
 }
