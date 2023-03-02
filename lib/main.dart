896748a4fcfa26825ee91c6de2548befbad70647
rianllauo <rianllauo@gmail.com>
2023-03-02 09:13:28 +0700

    bottom sheet bar

--- lib/main.dart
+++ lib/main.dart
@@ -24,48 +24,59 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int? indexTab;
-
-  late List showWidget = [
-    Center(
-      child: Text('Home'),
-    ),
-    Center(
-      child: Text('Cart'),
-    ),
-    Center(
-      child: Text('User'),
-    ),
-  ];
-
-  @override
-  void initState() {
-    indexTab = 0;
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          toolbarHeight: 70,
-          backgroundColor: Colors.blue,
-          title: const Text('Bottom Navigation Bar'),
+      appBar: AppBar(
+        toolbarHeight: 70,
+        backgroundColor: Colors.blue,
+        title: const Text('Bottom Navigation Bar'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(30),
+          child: ElevatedButton(
+              onPressed: () {
+                showModalBottomSheet(
+                  isDismissible: false,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(15),
+                        topRight: Radius.circular(15)),
+                  ),
+                  context: context,
+                  builder: (context) {
+                    return Container(
+                      height: 250,
+                      padding: EdgeInsets.only(top: 20),
+                      child: ListView(
+                        children: [
+                          ListTile(
+                            leading: Icon(Icons.home),
+                            title: Text('Home'),
+                          ),
+                          ListTile(
+                            leading: Icon(Icons.shop),
+                            title: Text('Shop'),
+                          ),
+                          ListTile(
+                            leading: Icon(Icons.shopping_bag),
+                            title: Text('Cart'),
+                          ),
+                          ListTile(
+                            leading: Icon(Icons.cancel),
+                            title: Text('Cancle'),
+                            onTap: () => Navigator.pop(context),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                );
+              },
+              child: Text('Show Button Sheet')),
         ),
-        body: showWidget[indexTab!],
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: indexTab!,
-          selectedItemColor: Colors.red,
-          unselectedItemColor: Colors.black,
-          onTap: (value) => setState(() {
-            indexTab = value;
-          }),
-          items: [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.shopping_cart), label: 'Cart'),
-            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'User'),
-          ],
-        ));
+      ),
+    );
   }
 }
